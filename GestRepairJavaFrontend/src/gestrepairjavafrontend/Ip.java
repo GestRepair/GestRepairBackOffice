6724282844871ce9af2259d4ef4e12a30b34ca56
barcelosrui <barcelos.rui@gmail.com>
2017-05-30 02:43:31 +0100

    IP

---
+++ GestRepairJavaFrontend/src/gestrepairjavafrontend/Ip.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestrepairjavafrontend;
+
+/**
+ *
+ * @author Convite
+ */
+public class Ip{
+    public String stIp(){
+     return "http://127.0.0.1:8080";
+    }
+}
