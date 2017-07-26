418f1da0796a82c8266423f8e2eef6a98932774b
barcelosrui <barcelos.rui@gmail.com>
2017-07-26 13:05:45 +0100

    Upload Photo finish ;)

--- GestRepairJavaFrontend/src/gestrepairjavafrontend/PostService.java
+++ GestRepairJavaFrontend/src/gestrepairjavafrontend/PostService.java
@@ -5,18 +5,31 @@
  */
 package gestrepairjavafrontend;
 
-import javax.swing.*;
 import java.net.*;
-import java.awt.image.*;
-import javax.imageio.*;
 import java.io.*;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.awt.image.*;
+import static java.lang.Math.log;
+import static java.lang.StrictMath.log;
+import javax.imageio.*;
+
+import static java.lang.System.currentTimeMillis;
+import static java.net.HttpURLConnection.HTTP_OK;
+import static java.net.URLConnection.guessContentTypeFromName;
+import static java.text.MessageFormat.format;
+
+import static java.util.logging.Level.INFO;
+import static java.util.logging.Logger.getLogger;
+
+import java.util.logging.Logger;
+import javax.swing.*;
+
 import javax.imageio.ImageIO;
+import static jdk.nashorn.internal.objects.NativeMath.log;
+import static jdk.nashorn.internal.objects.NativeMath.log;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import static sun.security.krb5.Confounder.bytes;
 
 /**
  *
@@ -25,62 +38,126 @@ import org.json.simple.parser.JSONParser;
 public class PostService {
 
     private static String LINE_FEED = "\r\n";
+    private static final String CHARSET = "UTF-8";
+    private long start;
 
     Ip ip = new Ip();
 
+    public URL Url() throws MalformedURLException {
+        URL url = new URL(ip.stIp() + "/service");
+        return url;
+    }
+
+    HttpURLConnection connection;
+    OutputStream outputStream;
+    PrintWriter writer;
+    String boundary;
+
     public void PostService(String login, String name, String price, String description, File photo) throws Exception {
+        //URL url = new URL(ip.stIp() + "/service");
+
+        final File uploadFile = photo;
+        try {
+            Connection(login);
+            addFormField("nameService", name);
+            addFormField("priceService", price);
+            addFormField("description", description);
+            addFilePart("photo", uploadFile);
+            final byte[] bytes = finish();
+            //final OutputStream os = new FileOutputStream("someoutput.txt");
+            //os.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void Connection(String login) throws UnsupportedEncodingException, ParseException, IOException {
+
+        start = currentTimeMillis();
+
+        boundary = "---------------------------" + currentTimeMillis();
+
         JSONObject newjson = (JSONObject) new JSONParser().parse(login);
         String user = newjson.get("login").toString();
         String pass = newjson.get("password").toString();
-        byte[] message = (user + ":" + pass).getBytes("UTF-8");
+        byte[] message = (user + ":" + pass).getBytes(CHARSET);
         String encoded = javax.xml.bind.DatatypeConverter.printBase64Binary(message);
-
-        URL url = new URL(ip.stIp() + "/service");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setConnectTimeout(5000);//5 secs
-        connection.setReadTimeout(5000);//5 secs
-        String boundary = Long.toHexString(System.currentTimeMillis());
-        connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-        connection.setRequestProperty("Accept", "multipart/form-data");
-        connection.setRequestProperty("Authorization", "Basic " + encoded);
+        connection = (HttpURLConnection) Url().openConnection();
+        connection.setConnectTimeout(15000);//15 secs
+        connection.setReadTimeout(10000);//10 secs
         connection.setRequestMethod("POST");
+        connection.setRequestProperty("Accept", CHARSET);
+        connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+        connection.setRequestProperty("Authorization", "Basic " + encoded);
+        connection.setUseCaches(false);
+        connection.setDoInput(true);
         connection.setDoOutput(true);
+        outputStream = connection.getOutputStream();
 
-        PrintWriter writer = null;
+        writer = new PrintWriter(new OutputStreamWriter(outputStream, CHARSET), true);
+    }
 
-        try {
-            writer = new PrintWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
-            String fileName = photo.getName();
-            writer.append("--" + boundary).append(LINE_FEED);
-            writer.append("Content-Disposition: form-data; name=\"file\"; filename=\"" + fileName + "\"").append(LINE_FEED);
-            writer.append("Content-Type: text/plain").append(LINE_FEED);
-            writer.append(LINE_FEED);
-            BufferedReader reader = null;
-            try {
-                reader = new BufferedReader(new InputStreamReader(new FileInputStream(photo), "UTF-8"));
-                for (String line; (line = reader.readLine()) != null;) {
-                    writer.append(line).append(LINE_FEED);
-                }
-            } finally {
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException logOrIgnore) {
-                    }
-                }
+    public void addFormField(final String name, final String value) {
+        writer.append("--").append(boundary).append(LINE_FEED)
+                .append("Content-Disposition: form-data; name=\"").append(name)
+                .append("\"").append(LINE_FEED)
+                .append("Content-Type: text/plain; charset=").append(CHARSET)
+                .append(LINE_FEED).append(LINE_FEED).append(value).append(LINE_FEED);
+    }
+
+    public void addFilePart(final String fieldName, final File uploadFile)
+            throws IOException {
+        final String fileName = uploadFile.getName();
+        writer.append("--").append(boundary).append(LINE_FEED)
+                .append("Content-Disposition: form-data; name=\"")
+                .append(fieldName).append("\"; filename=\"").append(fileName)
+                .append("\"").append(LINE_FEED).append("Content-Type: ")
+                .append(guessContentTypeFromName(fileName)).append(LINE_FEED)
+                .append("Content-Transfer-Encoding: binary").append(LINE_FEED)
+                .append(LINE_FEED);
+
+        writer.flush();
+        outputStream.flush();
+        try (final FileInputStream inputStream = new FileInputStream(uploadFile);) {
+            final byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
             }
-            writer.append(LINE_FEED);
-            writer.append("--" + boundary + "--").append(LINE_FEED);
-            writer.append(LINE_FEED);
-            writer.flush();
-            writer.close();
-        } catch (Exception e) {
-            System.out.append("Exception writing file" + e);
-        } finally {
-            if (writer != null) {
-                writer.close();
-            }
+            outputStream.flush();
         }
 
+        writer.append(LINE_FEED);
+    }
+
+    public void addHeaderField(String name, String value) {
+        writer.append(name).append(": ").append(value).append(LINE_FEED);
+    }
+
+    public byte[] finish() throws IOException {
+        writer.append(LINE_FEED).append("--").append(boundary).append("--")
+                .append(LINE_FEED);
+        writer.close();
+
+        final int status = connection.getResponseCode();
+        if (status != HTTP_OK) {
+            throw new IOException(format("{0} failed with HTTP status: {1}",
+                    Url(), status));
+        }
+
+        try (final InputStream is = connection.getInputStream()) {
+            final ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            final byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                bytes.write(buffer, 0, bytesRead);
+            }
+
+            log(INFO, format("{0} took {4} ms", Url(), (currentTimeMillis() - start)));
+            return bytes.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
     }
 }
